c575a556a925cdf815e250fd38c1e9081019df1e
Teklu Mezgebo <mezge002@umn.edu>
2024-02-21 22:30:31 -0600

    redux store created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
-  // write your createStore code here
+export function createStore(reducer) {
+  
+  let state
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+    return state
+  }
+
+  function getState() {
+    return state
+  }
+
+  return { dispatch, getState }
 }
 
 export function candyReducer(state = [], action) {
@@ -12,6 +25,8 @@ export function candyReducer(state = [], action) {
       return state;
   }
 }
+
+let store = createStore(candyReducer)
 
 function render() {
   let container = document.getElementById("container");
@@ -24,3 +39,4 @@ function render() {
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+store.dispatch({action: '@@INT'})
